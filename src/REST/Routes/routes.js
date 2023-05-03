2fb0c77a8ae245c32001b49760f9394fbb7d9b1b
llllllll-l <vincent.werelius@gmail.com>
2023-05-03 11:37:04 +0200

    CRUD

--- src/REST/Routes/routes.js
+++ src/REST/Routes/routes.js
@@ -3,9 +3,25 @@
 const express = require('express');
 const api = express.Router();
 
+// read
 api.get('/', (req, res) => {
     res.send({
         message: 'Hello World!'
     });
 });
+
+// create
+api.post('/', (req, res) => {
+    
+})
+
+// delete
+api.delete('/', (req, res) => {
+
+}) 
+
+// update
+api.patch('/', (req, res) => {
+
+})
 module.exports = api;
