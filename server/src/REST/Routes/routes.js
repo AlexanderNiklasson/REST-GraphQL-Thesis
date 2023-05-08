174e0fcc20e1b54d202554068844e8cd9821db2d
llllllll-l <vincent.werelius@gmail.com>
2023-05-08 13:19:28 +0200

    REST routes (tested)

--- server/src/REST/Routes/routes.js
+++ server/src/REST/Routes/routes.js
@@ -49,7 +49,7 @@ api.get('/movies', (req, res) => {
     })
 })
 
-// Fetch a rreview by id
+// Fetch a review by id
 api.get('/review', (req, res) => {
     Review.findById(req.query.id).populate('movie').populate('user').exec().then(review => {
         if (!review) {
@@ -100,6 +100,51 @@ api.post('/movie', (req, res) => {
         console.error('Error adding a new movie to the database: ', error);
         res.status(500).json({ error: 'Internal server error' });
     })
+})
+
+// Adding review on movie
+api.post('/review', (req, res) => {
+    let new_review = new Review({
+        title: req.body.title,
+        description: req.body.description,
+        body: req.body.body,
+        movie: req.body.movieID,
+        user: req.body.userID
+    })
+    new_review.save().then(async saved_review => {
+        console.log(saved_review);
+        await User.findById(saved_review.user.toString()).then(user => {
+            if (!user) {
+                res.status(404).json({ error: 'No such user exists' });
+            } else {
+                user.reviews.push(saved_review);
+                user.save();
+            }
+        }).catch(error => {
+            console.error('Error fetching user: ', error);
+            res.status(500).json({ error: 'Internal server error' });
+        })
+
+        await Movie.findById(saved_review.movie.toString()).then(movie => {
+            if (!movie) {
+                res.status(404).json({ error: 'No such movie exists yet in the database' });
+            } else {
+                movie.reviews.push(saved_review);
+                movie.save();
+                 // return postman
+            }
+        })
+        res.json(saved_review);
+    }).catch(error => {
+        console.error('Error adding the review: ', error);
+        res.status(500).json({ error: 'Internal server error' });
+    })
+})
+// POSTT END //
+
+// UPDATE START //
+api.put('/user', (req, res) => {
+    User.findByIdAndUpdate(req.body.userId)
 })
 
 
